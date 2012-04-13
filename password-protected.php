5b3c85dca1178398001bd3df2b9c4b15693fd423
Ben Huson <ben@thewhiteroom.net>
2012-04-13 21:06:48 +0100

    Use cookies instead of sessions.

--- password-protected.php
+++ password-protected.php
@@ -27,10 +27,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// Start session if not already started...
-if ( session_id() == '' )
-	session_start();
-
 // Setup Password Protected
 global $Password_Protected;
 $Password_Protected = new Password_Protected();
@@ -67,6 +63,8 @@ class Password_Protected {
 	
 	/**
 	 * Disable Feeds
+	 *
+	 * @todo An option/filter to prevent disabling of feeds.
 	 */
 	function disable_feeds() {
 		add_action( 'do_feed', array( $this, 'disable_feed' ), 1 );
@@ -93,18 +91,28 @@ class Password_Protected {
 			$pwd = get_option( 'password_protected_password' );
 			// If correct password...
 			if ( md5( $password_protected_pwd ) == $pwd && $pwd != '' ) {
-				$_SESSION[$this->get_site_id() . '_password_protected_auth'] = 1;
+				$this->set_auth_cookie();
+				if ( ! empty( $_REQUEST['redirect_to'] ) ) {
+					wp_redirect( $_REQUEST['redirect_to'] );
+					exit;
+				}
 			} else {
 				// ... otherwise incorrect password
+				$this->clear_auth_cookie();
 				$this->errors->add( 'incorrect_password', 'Incorrect Password' );
 			}
 		}
 		
 		// Log out
-		if ( isset( $_REQUEST['action'] ) && $_REQUEST['action'] == 'logout' ) {
-			$_SESSION[$this->get_site_id() . '_password_protected_auth'] = 0;
-			$this->errors = new WP_Error();
-			$this->errors->add( 'logged_out', __( 'You are now logged out.' ), 'message' );
+		if ( isset( $_REQUEST['password-protected'] ) && $_REQUEST['password-protected'] == 'logout' ) {
+			$this->logout();
+			$redirect_to = remove_query_arg( array( 'password-protected', 'redirect_to' ), ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
+			$query = array(
+				'password-protected' => 'login',
+				'redirect_to' => urlencode( $redirect_to )
+			);
+			wp_redirect( add_query_arg( $query, home_url() ) );
+			exit();
 		}
 	}
 	
@@ -117,12 +125,21 @@ class Password_Protected {
 			return;
 		
 		// Logged in
-		if ( isset( $_SESSION[$this->get_site_id() . '_password_protected_auth'] ) && $_SESSION[$this->get_site_id() . '_password_protected_auth'] == 1 )
+		if ( $this->validate_auth_cookie() )
 			return;
 		
 		// Show login form
-		include( dirname( __FILE__ ) . '/theme/login.php' );
-		exit();
+		if ( isset( $_GET['password-protected'] ) && 'login' == $_GET['password-protected'] ) {
+			include( dirname( __FILE__ ) . '/theme/login.php' );
+			exit();
+		} else {
+			$query = array(
+				'password-protected' => 'login',
+				'redirect_to' => urlencode( ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] )
+			);
+			wp_redirect( add_query_arg( $query, home_url() ) );
+			exit();
+		}
 	}
 	
 	/**
@@ -130,7 +147,127 @@ class Password_Protected {
 	 */
 	function get_site_id() {
 		global $blog_id;
-		return apply_filters( 'password_protected_blog_id', $blog_id );
+		return 'bid_' . apply_filters( 'password_protected_blog_id', $blog_id );
+	}
+	
+	/**
+	 * Logout
+	 */
+	function logout() {
+		$this->clear_auth_cookie();
+		do_action( 'password_protected_logout' );
+	}
+	
+	/**
+	 * Validate Auth Cookie
+	 */
+	function validate_auth_cookie( $cookie = '', $scheme = '' ) {
+		if ( ! $cookie_elements = $this->parse_auth_cookie( $cookie, $scheme ) ) {
+			do_action( 'password_protected_auth_cookie_malformed', $cookie, $scheme );
+			return false;
+		}
+		extract( $cookie_elements, EXTR_OVERWRITE );
+		
+		$expired = $expiration;
+	
+		// Allow a grace period for POST and AJAX requests
+		if ( defined( 'DOING_AJAX' ) || 'POST' == $_SERVER['REQUEST_METHOD'] )
+			$expired += 3600;
+	
+		// Quick check to see if an honest cookie has expired
+		if ( $expired < time() ) {
+			do_action('password_protected_auth_cookie_expired', $cookie_elements);
+			return false;
+		}
+		
+		$pass = md5( get_option( 'password_protected_password' ) );
+		$pass_frag = substr( $pass, 8, 4 );
+	
+		$key = md5( $this->get_site_id() . $pass_frag . '|' . $expiration );
+		$hash = hash_hmac( 'md5', $this->get_site_id() . '|' . $expiration, $key);
+		
+		if ( $hmac != $hash ) {
+			do_action( 'password_protected_auth_cookie_bad_hash', $cookie_elements );
+			return false;
+		}
+	
+		if ( $expiration < time() ) // AJAX/POST grace period set above
+			$GLOBALS['login_grace_period'] = 1;
+	
+		return true;
+	}
+
+	/**
+	 * Generate Auth Cookie
+	 */
+	function generate_auth_cookie( $expiration, $scheme = 'auth' ) {
+		$pass = md5( get_option( 'password_protected_password' ) );
+		$pass_frag = substr( $pass, 8, 4 );
+		
+		$key = md5( $this->get_site_id() . $pass_frag . '|' . $expiration );
+		$hash = hash_hmac( 'md5', $this->get_site_id() . '|' . $expiration, $key );
+		$cookie = $this->get_site_id() . '|' . $expiration . '|' . $hash;
+	
+		return $cookie;
+	}
+	
+	/**
+	 * Parse Auth Cookie
+	 */
+	function parse_auth_cookie( $cookie = '', $scheme = '' ) {
+		if ( empty( $cookie ) ) {
+			$cookie_name = $this->cookie_name();
+	
+			if ( empty( $_COOKIE[$cookie_name] ) )
+				return false;
+			$cookie = $_COOKIE[$cookie_name];
+		}
+	
+		$cookie_elements = explode( '|', $cookie );
+		if ( count( $cookie_elements ) != 3 )
+			return false;
+	
+		list( $site_id, $expiration, $hmac ) = $cookie_elements;
+	
+		return compact( 'site_id', 'expiration', 'hmac', 'scheme' );
+	}
+	
+	/**
+	 * Set Auth Cookie
+	 * @todo
+	 */
+	function set_auth_cookie( $remember = false, $secure = '') {
+		if ( $remember ) {
+			$expiration = $expire = time() + apply_filters( 'password_protected_auth_cookie_expiration', 86400, $remember );
+		} else {
+			$expiration = time() + apply_filters( 'password_protected_auth_cookie_expiration', 86400, $remember );
+			$expire = 0;
+		}
+	
+		if ( '' === $secure )
+			$secure = is_ssl();
+	
+		$secure_password_protected_cookie = apply_filters( 'password_protected_secure_password_protected_cookie', false, $secure );
+		$password_protected_cookie = $this->generate_auth_cookie( $expiration, 'password_protected' );
+		
+		setcookie( $this->cookie_name(), $password_protected_cookie, $expire, COOKIEPATH, COOKIE_DOMAIN, $secure_password_protected_cookie, true );
+		if ( COOKIEPATH != SITECOOKIEPATH )
+			setcookie( $this->cookie_name(), $password_protected_cookie, $expire, SITECOOKIEPATH, COOKIE_DOMAIN, $secure_password_protected_cookie, true );
+	}
+	
+	/**
+	 * Clear Auth Cookie
+	 */
+	function clear_auth_cookie() {
+		setcookie( $this->cookie_name(), ' ', time() - 31536000, COOKIEPATH, COOKIE_DOMAIN );
+		setcookie( $this->cookie_name(), ' ', time() - 31536000, SITECOOKIEPATH, COOKIE_DOMAIN );
+	}
+	
+	/**
+	 * Cookie Name
+	 */
+	function cookie_name() {
+		return $this->get_site_id() . '_password_protected_auth';
 	}
 	
 	/**
