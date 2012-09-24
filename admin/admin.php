3714e60541c9a6a4d6e59bef54bcb1664853dec8
Ben Huson <ben@thewhiteroom.net>
2012-09-24 21:04:15 +0100

    Use a filter to MD5 password before saving so that developers can intercept the plaintext password earlier if needed.

--- admin/admin.php
+++ admin/admin.php
@@ -8,6 +8,7 @@ class Password_Protected_Admin {
 	function Password_Protected_Admin() {
 		add_action( 'admin_init', array( $this, 'privacy_settings' ) );
 		add_action( 'admin_notices', array( $this, 'password_protected_admin_notices' ) );
+		add_filter( 'pre_update_option_password_protected_password', array( $this, 'pre_update_option_password_protected_password' ), 10, 2 );
 	}
 	
 	/**
@@ -54,7 +55,7 @@ class Password_Protected_Admin {
 				return $old_val;
 			} elseif ( $val['new'] == $val['confirm'] ) {
 				add_settings_error( 'password_protected_password', 'password_protected_password', __( 'New password saved.', 'password_protected' ), 'updated' );
-				return md5( $val['new'] );
+				return $val['new'];
 			}
 			return get_option( 'password_protected_password' );
 		}
@@ -81,6 +82,24 @@ class Password_Protected_Admin {
 	function password_protected_password_field() {
 		echo '<input type="password" name="password_protected_password[new]" id="password_protected_password_new" size="16" value="" autocomplete="off"> <span class="description">' . __( 'If you would like to change the password type a new one. Otherwise leave this blank.', 'password_protected' ) . '</span><br>
 			<input type="password" name="password_protected_password[confirm]" id="password_protected_password_confirm" size="16" value="" autocomplete="off"> <span class="description">' . __( 'Type your new password again.', 'password_protected' ) . '</span>';
+	}
+	
+	/**
+	 * Pre-update 'password_protected_password' Option
+	 *
+	 * Before the password is saved, MD5 it!
+	 * Doing it in this way allows developers to intercept with an earlier filter if they
+	 * need to do something with the plaintext password.
+	 *
+	 * @param string $newvalue New Value.
+	 * @param string $oldvalue Old Value.
+	 * @return string Filtered new value.
+	 */
+	function pre_update_option_password_protected_password( $newvalue, $oldvalue ) {
+		if ( $newvalue != $oldvalue ) {
+			$newvalue = md5( $newvalue );
+		}
+		return $newvalue;
 	}
 	
 	/**
