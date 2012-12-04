99182bdeed8041c991e8b894b0b82f2947cd2783
Ben Huson <ben@thewhiteroom.net>
2012-12-04 18:00:11 +0000

    Added pointer functionality to highlight recent features.

--- admin/js/settings.js
+++ admin/js/settings.js
@@ -9,5 +9,24 @@ jQuery(document).ready( function($) {
 			$('input#password_protected_feeds').attr('disabled', 'disabled');
 		}
 	}).trigger('change');
+ 
+	// Pointers
+	if (typeof(jQuery().pointer) != 'undefined') {
+		if (Password_Protected_Settings.pointerTextAllowAdministrators != '') {
+			jQuery('#password_protected_administrators').pointer({
+				content : Password_Protected_Settings.pointerTextAllowAdministrators,
+				position : {
+					edge: 'bottom',
+					align: 'left'
+				},
+				close   : function() {
+					jQuery.post( ajaxurl, {
+						pointer : 'PASSWORD_PROTECTED_ALLOW_ADMINISTRATORS',
+						action  : 'dismiss-wp-pointer'
+					});
+				}
+			}).pointer('open');
+		}
+	}
 
 });
