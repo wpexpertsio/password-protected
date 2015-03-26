e3047dda65b556829ebf5e66c00871e4903f20fe
Ben Huson <ben@thewhiteroom.net>
2015-03-26 08:27:04 +0000

    Version 2.0 (again)

--- password-protected.php
+++ password-protected.php
@@ -4,7 +4,7 @@
 Plugin Name: Password Protected
 Plugin URI: https://wordpress.org/plugins/password-protected/
 Description: A very simple way to quickly password protect your WordPress site with a single password. Please note: This plugin does not restrict access to uploaded files and images and does not work on WP Engine or with some caching setups.
-Version: 1.9
+Version: 2.0
 Author: Ben Huson
 Text Domain: password-protected
 Author URI: http://github.com/benhuson/password-protected/
